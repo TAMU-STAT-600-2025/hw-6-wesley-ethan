2c7602355821c522d30ddeffc74656abaaf57730
Wesley Halstead <wesley.halstead@tamu.edu>
2025-11-10 11:25:46 -0600

    Add input checks to Kmeans

--- R/Kmeans_wrapper.R
+++ R/Kmeans_wrapper.R
@@ -14,6 +14,15 @@ MyKmeans <- function(X, K, M = NULL, numIter = 100){
   
   n = nrow(X) # number of rows in X
   
+  # Check that X is a matrix, K and numIter are positive integers, M is either a matrix or NULL, and that K < n
+  stopifnot(
+    "X must be a numeric matrix" = is.numeric(X) && is.matrix(X),
+    "K must be a positive integer" = is.numeric(K) && (K %% 1 == 0) && (K > 0),
+    "numIter must be a positive integer" = is.numeric(K) && (numIter %% 1 == 0) && (numIter > 0),
+    "M must be a numeric matrix or of value NULL" = any(is.numeric(M) && is.matrix(M), is.null(M)),
+    "Number of rows in X must be greater than or equal to number of clusters, K" = (n >= K)
+  )
+  
   # Check whether M is NULL or not. If NULL, initialize based on K random points from X. If not NULL, check for compatibility with X dimensions.
   
   
