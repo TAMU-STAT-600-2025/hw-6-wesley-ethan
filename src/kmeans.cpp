bbb4ba133ba28f47983610385a4d860de8a1d13a
Wesley Halstead <wesley.halstead@tamu.edu>
2025-11-10 14:24:57 -0600

    Implement main loop of kmeans.cpp

--- src/kmeans.cpp
+++ src/kmeans.cpp
@@ -19,11 +19,41 @@ arma::uvec MyKmeans_c(const arma::mat& X, int K,
     arma::uvec Y(n); // to store cluster assignments
     
     // Initialize any additional parameters if needed
+    arma::vec ones_n(n, arma::fill::ones);
+    arma::vec ones_k(K, arma::fill::ones);
+    arma::mat M_new = M;
+    arma::mat M_old = M;
+    int sum_unique = 0.5 * (K - 1) * K;
     
     // For loop with kmeans algorithm
-    
+    for (int i = 0; i < numIter; i++){
+      // Calculate distance matrix
+      arma::mat distance = (arma::sum(X % X, 1) * ones_k.t()) + (ones_n * arma::sum(M_new % M_new, 1).t()) - 2 * X * M_new.t();
+      
+      // Update each classification
+      for (int r = 0; r < n; r++){
+        Y(r) = distance.row(r).index_min();
+      }
+      
+      // Stop if a cluster disappears
+      if (arma::accu(arma::unique(Y)) < sum_unique) {
+         Rcpp::stop("One of the clusters has disappeared, indicating a poor starting point. Give a new matrix, M, and try again.");
+      }
+      
+      for (int cl = 0; cl < K; cl++) {
+        // Update each cluster means
+        int n_cl = arma::accu(Y == cl);
+        M_new.row(cl) = arma::sum(X.rows(arma::find(Y == cl)))/n_cl;
+      }
+      
+      if (arma::accu(M_old - M_new) == 0) {
+        break;
+      }
+      
+      M_old = M_new;
+    }
     
     // Returns the vector of cluster assignments
-    return(Y);
+    return(Y + 1);
 }
 
