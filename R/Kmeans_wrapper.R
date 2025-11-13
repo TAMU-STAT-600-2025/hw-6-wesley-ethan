7198c9f277a716e49d105d8ef09c859811e7ed8a
Wesley Halstead <wesley.halstead@tamu.edu>
2025-11-13 09:21:13 -0600

    Ran devtools::document to update documentation

--- R/Kmeans_wrapper.R
+++ R/Kmeans_wrapper.R
@@ -1,8 +1,8 @@
 #' K-Means Clustering Algorithm
 #'
-#' @param X $n \times p$ matrix containing $n$ data points to cluster
+#' @param X n \times p matrix containing $n$ data points to cluster
 #' @param K integer specifying the number of clusters
-#' @param M (optional) $K \times p$ matrix of cluster centers
+#' @param M (optional) K \times p matrix of cluster centers
 #' @param numIter number of maximal iterations for the algorithm, the default value is 100
 #'
 #' @return Returns a vector of length $n$ containing class assignments for each data point.
@@ -14,7 +14,6 @@
 #' 
 #' # Cluster data around centers
 #' MyKmeans(X, 2, M = rbind(rep(10, 5), rep(-10, 5))
-#' 
 MyKmeans <- function(X, K, M = NULL, numIter = 100){
   
   n = nrow(X) # number of rows in X
